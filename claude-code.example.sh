2716bd0d163cea199acdea6333d72062913eb588
Conan <chongconan320@gmail.com>
2026-06-21 01:19:11 +0800

    COrs and remove sentsitive env

--- claude-code.example.sh
+++ claude-code.example.sh
@@ -1,5 +1,5 @@
 export ANTHROPIC_BASE_URL=https://api.deepseek.com/anthropic
-export ANTHROPIC_AUTH_TOKEN=sk-3f21b4ca779249f2a336916b5cbdf7c1
+export ANTHROPIC_AUTH_TOKEN=
 export ANTHROPIC_MODEL=deepseek-v4-pro[1m]
 export ANTHROPIC_DEFAULT_OPUS_MODEL=deepseek-v4-pro[1m]
 export ANTHROPIC_DEFAULT_SONNET_MODEL=deepseek-v4-pro[1m]
