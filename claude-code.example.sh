1933fc9137c1f517deebe8b8f676cedce865009e
Conan <chongconan320@gmail.com>
2026-06-21 00:46:06 +0800

    feat: Claude Code deepseek example

---
+++ claude-code.example.sh
@@ -0,0 +1,8 @@
+export ANTHROPIC_BASE_URL=https://api.deepseek.com/anthropic
+export ANTHROPIC_AUTH_TOKEN=sk-3f21b4ca779249f2a336916b5cbdf7c1
+export ANTHROPIC_MODEL=deepseek-v4-pro[1m]
+export ANTHROPIC_DEFAULT_OPUS_MODEL=deepseek-v4-pro[1m]
+export ANTHROPIC_DEFAULT_SONNET_MODEL=deepseek-v4-pro[1m]
+export ANTHROPIC_DEFAULT_HAIKU_MODEL=deepseek-v4-flash
+export CLAUDE_CODE_SUBAGENT_MODEL=deepseek-v4-flash
+export CLAUDE_CODE_EFFORT_LEVEL=max
